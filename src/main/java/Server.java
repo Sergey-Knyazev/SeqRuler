9fae49660043cb40899f2bfd5441a78e1c0eb50b
Sergey-Knyazev <sergey.n.knyazev@gmail.com>
2018-10-19 16:25:20 -0400

    wip

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,21 +1,40 @@
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
 @ServerEndpoint("/")
 public class Server {
-    public Server(){}
+    public Server(){System.out.println("class loaded " + this.getClass());
+    }
 
     @OnOpen
-    public void onOpen(Session session) {}
+    public void onOpen(Session session) {
+        System.out.printf("Session opened, id: %s%n", session.getId());
+        try {
+            session.getBasicRemote().sendText("Hi there, we are successfully connected.");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     @OnMessage
     public void onMessage(String message, Session session) {
-
+        System.out.printf("Message received. Session id: %s Message: %s%n",
+                session.getId(), message);
+        try {
+            session.getBasicRemote().sendText(String.format("We received your message: %s%n", message));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @OnError
-    public void onError(Throwable e){e.printStackTrace();}
+    public void onError(Throwable e) {
+        e.printStackTrace();
+    }
 
     @OnClose
-    public void onClose(Session session) {}
+    public void onClose(Session session) {
+        System.out.printf("Session closed with id: %s%n", session.getId());
+    }
 }
