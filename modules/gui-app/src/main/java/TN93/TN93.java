bbac9bac5647ab3787aad644e41aa97fd621ebf1
Sergey-Knyazev <sergey.n.knyazev@gmail.com>
2018-12-07 17:38:22 -0500

    accuracy improved

--- modules/gui-app/src/main/java/TN93/TN93.java
+++ modules/gui-app/src/main/java/TN93/TN93.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.Math.log;
 
@@ -31,7 +32,14 @@ public class TN93 {
 
     public static double[][] tn93(LinkedList<Seq> seqs) {
         double[][] dist = new double[seqs.size()][seqs.size()];
+        long startTime = System.nanoTime(), estimatedTime;
         for (int i = 1; i < dist.length; ++i) {
+            if(i % 1000 == 0) {
+                estimatedTime = System.nanoTime() - startTime;
+                System.out.print(String.format("%d out of %d for ", i, dist.length));
+                System.out.print(TimeUnit.SECONDS.convert(estimatedTime, TimeUnit.NANOSECONDS));
+                System.out.println(" sec");
+            }
             for (int j = 0; j < i; ++j) {
                 dist[i][j] = dist[j][i] = tn93(seqs.get(i).getSeq_enc(), seqs.get(j).getSeq_enc());
             }
@@ -40,7 +48,7 @@ public class TN93 {
     }
 
     private static double tn93(int[] s1, int[] s2) {
-        int n = 0;
+        int total_non_gap = 0;
         int[] nucl_counts = new int[4];
         int[][] nucl_pair_count = new int[4][4];
 
@@ -52,23 +60,30 @@ public class TN93 {
             ++nucl_counts[c2];
             ++nucl_pair_count[c1][c2];
             ++nucl_pair_count[c2][c1];
-            ++n;
+            ++total_non_gap;
         }
         double[] nucl_freq = new double[4];
-        for(int i=0; i<4; ++i) nucl_freq[i] = (double) nucl_counts[i]/2/n;
-        double p1 = (double) nucl_pair_count[Seq.A][Seq.G]/n;
-        double p2 = (double) nucl_pair_count[Seq.C][Seq.T]/n;
+        for(int i=0; i<4; ++i) nucl_freq[i] = (double) nucl_counts[i]/2/total_non_gap;
+        double p1 = (double) nucl_pair_count[Seq.A][Seq.G]/total_non_gap;
+        double p2 = (double) nucl_pair_count[Seq.C][Seq.T]/total_non_gap;
         double q = ((double) nucl_pair_count[Seq.A][Seq.C]+nucl_pair_count[Seq.A][Seq.T]+nucl_pair_count[Seq.C][Seq.G]+
-                nucl_pair_count[Seq.C][Seq.T]+nucl_pair_count[Seq.G][Seq.T])/n;
+                nucl_pair_count[Seq.G][Seq.T])/total_non_gap;
         double g_r = nucl_freq[Seq.A]+nucl_freq[Seq.G];
-        double g_y = nucl_freq[Seq.A]+nucl_freq[Seq.G];
-        double k_ag = 2*nucl_freq[Seq.A]*nucl_freq[Seq.G]/g_r;
-        double k_tc = 2*nucl_freq[Seq.T]*nucl_freq[Seq.C]/g_y;
-        double k_ry = 2*g_r*g_y;
+        double g_y = nucl_freq[Seq.C]+nucl_freq[Seq.T];
+        boolean useK2P = false;
+        for(int i=0; i<4; ++i) if (nucl_freq[i] == 0) useK2P = true;
+        if (useK2P) {
+            //TODO:
+            System.out.println("Hi");
+            return 0;
+        }
+        double k_ag = nucl_freq[Seq.A]*nucl_freq[Seq.G]/g_r;
+        double k_tc = nucl_freq[Seq.T]*nucl_freq[Seq.C]/g_y;
+        double k_ry = g_r*g_y;
 
-        return -k_ag*log(1-p1/k_ag-q/(2*g_r))
-                -k_tc*log(1-p2/k_tc-q/(2*g_y))
-                -(k_ry-k_ag*g_y-k_tc*g_r)*log(1-q/k_ry);
+        return -2*k_ag*log(1-p1/(2*k_ag)-q/(2*g_r))
+                -2*k_tc*log(1-p2/(2*k_tc)-q/(2*g_y))
+                -2*(k_ry-k_ag*g_y-k_tc*g_r)*log(1-q/(2*k_ry));
     }
 
     public static LinkedList<Seq> read_seqs(Scanner sc) {
